2af97cf9ee84c7d8d86dcfca6ea1b8e2311a0801
desiderataaus <humphry.au@gmail.com>
2016-10-18 09:59:11 +1100

    Update test cases

--- uritest.cpp
+++ uritest.cpp
@@ -5,38 +5,81 @@
 
 using namespace std;
 
-int main(int argc, char* argv[])
+void DontDelete() {;}
+
+void ParseAndDispay(std::unique_ptr<URIParser> pParser, string& URIstring)
 {
+    string result = "Not set";
 
-
-    if (1 == argc){
-        cout << "Usage: uriparser URI string to parse"<<endl;
-        cout << "For example, uriparser \"abc://username:password@example.com:123/path\""<<endl;
-        return 0;
+    if (pParser != nullptr){
+        
+        pParser->ParseIt(URIstring, result);
     }
 
-    string uristr = argv[1];
+    cout<<string(pParser->GetType())<<": "<<result<<endl;    
+}
 
-    string typelist[] = {"Scheme", "UserName", "Host", "Path", "Password", "Query", "Fragment"};
-    set<string> uritypes(begin(typelist), end(typelist));
-    
-    for (set<string>::iterator it=uritypes.begin(); it!=uritypes.end(); ++it){\
+void ParseURI(string& URIstring, set<string>& typeset)
+{
+    for (set<string>::iterator it=typeset.begin(); it!=typeset.end(); ++it){\
         
         string type = *it;
 
         std::unique_ptr<URIParser> pParser = URIParser::GetParser(type);
     
-        string result = "Not set";
-
-        if (pParser != nullptr){
-            pParser->ParseIt(uristr, result);
+        if (pParser == nullptr){
+            cout << type << " is not a valid type. Ignore it.";
+            return;
         }
 
-        cout<<type<<": "<<result<<endl;
+        ParseAndDispay(move(pParser), URIstring);
+    }    
+}
 
-        //delete pParser;
+int main(int argc, char* argv[])
+{
+
+
+    if (argc < 2){
+        cout << "Usage: uriparser \"URI string\" [type list]"<<endl;
+        cout << "For example, uriparser \"abc://username:password@example.com:123/path\" Scheme Host"<<endl;
+        return 0;
+    }
+
+    string uristr = "";
+    if (argc > 1){
+        uristr = argv[1];
+    }
+
+    set<string> uritypes;
+    if (argc > 2){
+        
+        for(int i = 2; i < argc; i++){
+            uritypes.insert(argv[i]);
+        }
+    
+        cout<<"Parsing URI string based on input typelist..."<<endl;
+
+        ParseURI(uristr, uritypes);
+
+        cout << endl;
     }
     
+    cout<<"Parsing URI string based on preset typelist..."<<endl;
+    uritypes.clear();
+
+    string typelist[] = {"Scheme", "UserName", "Host", "Path", "Password", "Query", "Fragment"};
+    uritypes = std::set<string>(begin(typelist), end(typelist));
+
+    ParseURI(uristr, uritypes);
+    cout << endl;
+
+    cout<<"Parsing URI string by specified types"<<endl;
+
+    ParseAndDispay(unique_ptr<URIParser>(new URIParserUserName), uristr);
+    ParseAndDispay(unique_ptr<URIParser>(new URIParserPassword), uristr);
+    cout << endl;
+   
 	return 0;
 }
 
